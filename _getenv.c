aeb227733b09da3fd640b66d7bc54c6c2393e155
Gabriel <fossecruor@gmail.com>
2022-12-07 05:32:46 -0800

    "ajout du _getenv.c"

--- _getenv.c
+++ _getenv.c
@@ -1,45 +1,21 @@
 #include "main.h"
 
-/**
- * _getenv- return the value of the environment variable NAME
- * @name: environment variable NAME
- *
- * Return: pointer to value of name or NULL if failed
- */
-
-
 char *_getenv(const char *name)
 {
+	char **g, *found = NULL, *token = NULL;
 	int i = 0;
-	char **g;
-	char *value_name = NULL, *match = NULL, *path = NULL;
-	unsigned int path_len;
 
-	g = environ; /* pointer to char externe environ */
-
+	g = environ;
 	while (g[i])
 	{
-		/* search line with NAME in environ */
-		match = strstr(g[i], name);
-
-		if (match)
+		found = strstr(g[i], name);
+		if (found)
 		{
-			/* split environ variable to get only part after = */
-			value_name = strtok(match, "=");
-			value_name = strtok(NULL, "\n");
-			if (value_name == NULL)
-				exit(0);
-			/* calcul of len of path to allocate dynamic memory */
-			path_len = strlen(value_name);
-			path = malloc(sizeof(char) * path_len + 1);
-			if (path == NULL)
-				return (NULL);
-			path = strcpy(path, value_name);
-
-			return (path);
+			token = strtok(found, "=");
+			token = strtok(NULL, "=");
+			return (token);
 		}
 		i++;
 	}
-
 	return (NULL);
 }
