2c0125827c8b55160f0910f313c9120f35754bdd
Arrue Marianne <5608@holbertonstudents.com>
2022-12-09 08:28:44 -0800

    bugfix env modification in print_env function

--- main.h
+++ main.h
@@ -13,29 +13,37 @@
 #include <sys/wait.h>
 #include <errno.h>
 
+/* error strings */
+#define ERROR_MALLOC "Cannot allocate memory with malloc\n"
+#define ERROR_FORK "Cannot fork and create child process\n"
+#define ERROR_PATH "No such file or directory\n"
+
+
 /**
- * struct node - single linkes list
- * @path_directory: string
- * @next: point to the next node
- * Description: singly linked list of node structure
+ * struct built_s - linked list of builtins
+ * @name of builtin
+ * @func: pointer to function 
+ * Description: struct for builtin functions
  */
 
-typedef struct node
+typedef struct builtin_s
 {
-	char *path_directory;
-	struct node *next;
-} node_t;
+	char *name;
+	int (*func)(void);
+}builtin_t;
 
-
+/* environment variable */
 extern char **environ;
 
+
+/* Function free all malloc */
 void free_malloc(char **token, char *line, char *fullpath, int fp_malloc);
+/* function like which */
 char *_which(char *cmd, char *fullpath, char *path_var);
+/* function like getenv */
 char *_getenv(char *name);
 /* liste all directory path */
 char *_directory_path(char *path);
-/* create linked list of path directory */
-node_t *_linkedlist_dir(char *path);
 /* add new variable of environment or replace value */
 int _setenv(const char *name, const char *value, int overwrite);
 /* construct NAME=VALUE for environment variable */
@@ -44,7 +52,12 @@ char *build_var_env(const char *name, const char *value);
 int _setenv(const char *name, const char *value, int overwrite);
 /* implicit declaration of getline so i define it*/
 ssize_t getline(char **lineptr, size_t *n, FILE *stream);
-void print_full_env(void);
+/* function print env */
+void print_full_env(char **envp);
 
+/* function for builin */
+int shell_env(void);
+int shell_exit(void);
+int builtin_execute(char **cmd);
 
 #endif
